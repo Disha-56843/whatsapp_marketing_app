71733a0fa25506841455c0951479110979f3b204
Disha Jasani <113577480+Disha-56843@users.noreply.github.com>
2026-03-05 12:53:49 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,9 +1,23 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("✅ MongoDB Connected");
+//   } catch (err) {
+//     console.error("❌ MongoDB Connection Error:", err.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error("❌ MongoDB Connection Error:", err.message);
     process.exit(1);
